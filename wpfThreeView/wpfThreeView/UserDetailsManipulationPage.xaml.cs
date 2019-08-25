4eecfec8d7e60401b3d3a26b767f6face1b5246a
dewmalFernando <dewmalfernando@gmail.com>
2019-08-25 22:03:55 +0530

    Add somes changes

--- wpfThreeView/wpfThreeView/UserDetailsManipulationPage.xaml.cs
+++ wpfThreeView/wpfThreeView/UserDetailsManipulationPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,17 +23,53 @@ namespace wpfThreeView
     /// </summary>
     public partial class UserDetailsManipulationPage : Page
     {
-        
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-JJMIDS9\MSSQL;Initial Catalog=LibraryManagementSystem;Integrated Security=True");
+
+        DB_MemberTableDataContext dB_MemberTable = new DB_MemberTableDataContext(Properties.Settings.Default.LibraryManagementSystemConnectionString);
 
         public UserDetailsManipulationPage()
         {
             InitializeComponent();
+
+            if (dB_MemberTable.DatabaseExists())
+                userDetailsManipulationDataGrid.ItemsSource = dB_MemberTable.Table_Members;
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             AdminPage adminPage = new AdminPage();
             NavigationService.Navigate(adminPage);
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            dB_MemberTable.SubmitChanges();
+        }
+
+        private void SerachTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Open sql connection
+            sqlConnection.Open();
+            //Create a new sql command
+            SqlCommand cmd = sqlConnection.CreateCommand();
+            //Define command type
+            cmd.CommandType = System.Data.CommandType.Text;
+            //Sql query for select enterd data from database
+            cmd.CommandText = "SELECT * FROM Book WHERE Title LIKE('" + SerachTextBox.Text + "%')";
+            //cmd.CommandText = "SELECT * FROM Book WHERE Author LIKE('" + SerachTextBox.Text + "')";
+            //Execute sql command
+            cmd.ExecuteNonQuery();
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            sqlDataAdapter.Fill(dataTable);
+            userDetailsManipulationDataGrid.ItemsSource = dataTable.DefaultView;
+            //close sql connection
+            sqlConnection.Close();
+        }
+
+        private void DeleteBookButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
